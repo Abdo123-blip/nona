2b72f4ce35cf786435905839ec309fbdf4deec9c
Abdelrahman <abdelrahmanelazab343@gmail.com>
2025-05-02 01:56:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDescription(text) {
+    document.getElementById('descText').innerText = text;
+    document.getElementById('descBox').style.display = 'block';
+}
+
+function hideDescription() {
+    document.getElementById('descBox').style.display = 'none';
+}
